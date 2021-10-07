d3f71410f8b2c5cb90691f9a835119467ac47fe3
Shyam Singh Nevula <shyamsinghnevula@Shyams-MacBook-Pro.local>
2021-10-07 17:41:58 +0530

    monorepo Practise

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # monorepo
+
+npm i -D lerna
+
+npx lerna init
+
+Change the Version Number in package.json and lerna.json
+
+navigate to packages and create react app by npx create-react-app appName.
+
---
+++ packages/server-api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("I am a Backend Server")
+})
+
+
+
+
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(`Error ${err.message}`)
+    }
+    else{ 
+        console.log(`Listening At Port ${port}`)
+    }
+})
